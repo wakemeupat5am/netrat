61768971a34f21708d0189046f28d0367bee20c5
wakemeupat05am <gabidenoverdrive@gmail.com>
2026-02-16 22:05:21 +0500

    Add files via upload

--- netrat.sh
+++ netrat.sh
@@ -11,27 +11,33 @@ echo -e "${BOLD}${BLUE}==================== NetRat Tool ====================${RE
 echo -e "${YELLOW}[!] Warning${RESET} Reconing without hosts permission is a crime"
 
 if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-  printf "%-16s | %-30s | %-8s | %-10s\n" "Domain/IP" "ORG" "Country" "CIDR"
-  printf "%-16s-+-%-30s-+-%-8s-+-%-10s\n" "---------------" "------------------------------" "--------" "----------"
+  printf "%-16s | %-16s | %-30s | %-8s | %-10s\n" "Domain" "IP" "ORG" "Country" "CIDR"
+  printf "%-16s-+-%-16s-+-%-30s-+-%-8s-+-%-10s\n" "----------------" "----------------" "------------------------------" "--------" "----------"
   for arg in "$@"; do 
-    org=$(whois "$arg" | grep -iPo '(Organization:   |ORG:  \s+)\K.*')
-    cedr=$(whois "$arg" | grep -iPo '(CIDR:           |CID:  \s+)\K.*')
-    country=$(whois "$arg" | grep -iPo '(Country:        |CNT:\s+)\K.*')
-    printf "%-16s | %-30s | %-8s | %-10s\n" "$arg" "$org" "$country" "$cedr"
+    org=$(whois "$arg" | grep -iPo '(Organization:   |ORG:  \s+)\K.*' | head -n 1)
+    cedr=$(whois "$arg" | grep -iPo '(CIDR:           |CID:  \s+)\K.*'  | head -n 1)
+    country=$(whois "$arg" | grep -iPo '(Country:        |CNT:\s+)\K.*'  | head -n 1)
+    printf "%-16s | %-16s | %-30s | %-8s | %-10s\n"  " " "$arg" "$org" "$country" "$cedr"
   done
-else
-  printf "%-16s | %-30s | %-8s | %-10s \n" "Domain/IP" "ORG" "Country" "RegDate"
-  printf "%-16s-+-%-30s-+-%-8s-+-%-10s\n" "---------------" "------------------------------" "--------" "----------"
-  if [ -n $1 ]; then
-    for arg in "$@"; do
-      org=$(whois "$arg" | grep -iPo '(Registrant Organization: |organization: |Organization Name......: |org:           \s+)\K.*')
-      sleep 1
-      country=$(whois "$arg" | grep -iPo '(Registrant Country: |Country................: |country:         \s+)\K.*')
-      sleep 1
-      regdate=$(whois "$arg" | grep -iPo '(Domain created: |Creation Date: |created:    \s+)\K.*')
-      printf "%-16s | %-30s | %-8s | %-10s\n" "$arg" "$org" "$country" "$regdate"
-    done
   fi
+echo "doing domain"
+if [ -n $1 ]; then
+  printf "%-16s | %-16s | %-30s | %-8s | %-10s \n" "Domain" "IP" "ORG" "Country" "RegDate"
+  printf "%-16s-+-%-16s-+-%-30s-+-%-8s-+-%-10s\n" "----------------" "----------------" "------------------------------" "--------" "----------"
+    for arg in "$@"; do
+      org=$(whois "$arg" | grep -iPo '(Registrant Organization: |organization: |Organization Name......: |org:           \s+)\K.*'  | head -n 1)
+      sleep 1
+      country=$(whois "$arg" | grep -iPo '(Registrant Country: |Country................: |country:         \s+)\K.*'  | head -n 1)
+      sleep 1
+      regdate=$(whois "$arg" | grep -iPo '(Domain created: |Creation Date: |created:    \s+)\K.*'  | head -n 1)
+      sleep 1
+      ipdig=$(dig +short "$arg" | head -n 1)
+      printf "%-16s | %-16s | %-30s | %-8s | %-10s\n" "$arg" "$ipdig" "$org" "$country" "$regdate"
+      dig +short "$arg" | tail -n +2 | while read -r ip; do
+        printf "%-16s | %-16s | %-30s | %-8s | %-10s\n" " " "$ip" " " " " " "
+      done
+
+    done
 fi
 
 
