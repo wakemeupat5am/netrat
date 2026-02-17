eb3163a516083693f87fe8709a61e83b6e670a3b
wakemeupat05am <gabidenoverdrive@gmail.com>
2026-02-17 13:36:21 +0500

    Add files via upload

--- netrat.sh
+++ netrat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+
 #Neccessary hacker wannabe script kiddy decorators
 RED="\033[31m"
 GREEN="\033[32m"
@@ -9,6 +10,13 @@ RESET="\033[0m"
 
 echo -e "${BOLD}${BLUE}==================== NetRat Tool ====================${RESET}"
 echo -e "${YELLOW}[!] Warning${RESET} Reconing without hosts permission is a crime"
+
+#WHOIS RECON SECTION AND JSON WRITE
+json='{
+  "domains": [
+    {"name": "example.com"}
+  ]
+}'
 
 if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
   printf "%-16s | %-16s | %-30s | %-8s | %-10s\n" "Domain" "IP" "ORG" "Country" "CIDR"
@@ -36,9 +44,25 @@ if [ -n $1 ]; then
       dig +short "$arg" | tail -n +2 | while read -r ip; do
         printf "%-16s | %-16s | %-30s | %-8s | %-10s\n" " " "$ip" " " " " " "
       done
-
     done
+  output="[]"
+  for domain in "$@"; do
+    ips=$(dig +short "$domain" | jq -R -s -c 'split("\n")[:-1]')
+    obj=$(jq -n --arg d "$domain" --argjson i "$ips" '{domain:$d, ips:$i}')
+    output=$(echo "$output" | jq ". + [$obj]")
+  done
+  echo "$output" > result.json
 fi
 
+#NMAPING IPS FOR VULNS
 
-
+FILE="result.json"
+jq -c '.[]' "$FILE" | while read -r item; do
+    domain=$(echo "$item" | jq -r '.domain')
+    echo -e "${BOLD}${BLUE}==================== NMAP-ing DOMAIN: $domain ====================${RESET}"
+    echo "$item" | jq -r '.ips[]' | while read -r ip; do
+        echo -e "${YELLOW} -> Scanning $ip ${RESET}"
+        nmap -Pn -T3 -p 1-1000 "$ip"
+        echo
+    done
+done
